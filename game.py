9c6e9df14561af61785ca67edcb837d6fd6f480c
Amber Orrange <orrangeamber@gmail.com>
2021-10-26 16:33:59 -0500

    player greeting

--- game.py
+++ game.py
@@ -1,3 +1,18 @@
 """A number-guessing game."""
 
 # Put your code here
+# greet player
+# get player name
+# choose random number between 1 and 100
+# repeat forever:
+#     get guess
+#     if guess is incorrect:
+#         give hint
+#         increase number of guesses
+#     else:
+#         congratulate player
+
+
+print("Welcome to the guessing number game.")
+player = input("What is your name?  ").upper()
+print(f"Great {player}, let's get started!")
